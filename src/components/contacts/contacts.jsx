fec972f7c2af2065fbe12ef52fd4e19d1a1e7701
WadimSh <wadim11@yandex.ru>
2023-03-22 14:23:16 +0300

    feat: сверстан блок contacts

--- src/components/contacts/contacts.jsx
+++ src/components/contacts/contacts.jsx
@@ -3,7 +3,29 @@ import style from './contacts.module.css';
 function Contacts() {
   return (
     <section className={style.wrapper}>
-      Contacts
+      <h2 className={style.title}>
+        // КОНТАКТЫ
+      </h2>
+      <div className={style.overlay}>
+        <ul className={style.list}>
+          <li className={style.item}>
+            <a className={style.link} href="">les-sib-rf@mail.ru</a>
+          </li>
+          <li className={style.item}>
+            <a className={style.link} href="">+7 (991) 922-78-82</a>
+          </li>
+          <li className={style.item}>
+            <a className={style.link} href="">
+              <div className={style.telegram}></div>
+            </a>
+          </li>
+          <li className={style.item}>
+            <a className={style.link} href="">
+            <div className={style.whatsapp}></div>
+            </a>
+          </li>
+        </ul>
+      </div>
     </section>
   );
 }
