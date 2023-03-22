f89eea1cb51ea87ffd96e30c6d5c1f1bc63a45eb
WadimSh <wadim11@yandex.ru>
2023-03-22 18:20:26 +0300

    feat: сверстан блок cover

--- src/components/contacts/contacts.jsx
+++ src/components/contacts/contacts.jsx
@@ -2,25 +2,25 @@ import style from './contacts.module.css';
 
 function Contacts() {
   return (
-    <section className={style.wrapper}>
+    <section className={style.wrapper} id="contacts">
       <h2 className={style.title}>
         // КОНТАКТЫ
       </h2>
       <div className={style.overlay}>
         <ul className={style.list}>
           <li className={style.item}>
-            <a className={style.link} href="">les-sib-rf@mail.ru</a>
+            <a className={style.link} href="mailto:les-sib-rf@mail.ru">les-sib-rf@mail.ru</a>
           </li>
           <li className={style.item}>
-            <a className={style.link} href="">+7 (991) 922-78-82</a>
+            <a className={style.link} href="tel:+7 (991) 922-78-82">+7 (991) 922-78-82</a>
           </li>
           <li className={style.item}>
-            <a className={style.link} href="">
+            <a className={style.link} href="https://t.me/lessibrf">
               <div className={style.telegram}></div>
             </a>
           </li>
           <li className={style.item}>
-            <a className={style.link} href="">
+            <a className={style.link} href="https://wa.me/79257302695">
             <div className={style.whatsapp}></div>
             </a>
           </li>
--- src/components/cover/cover.jsx
+++ src/components/cover/cover.jsx
@@ -1,9 +1,17 @@
 import style from './cover.module.css';
 
+import cover from '../../images/cover.jpg';
+
 function Cover() {
   return (
     <section className={style.wrapper}>
-      Cover
+      <div className={style.description}>
+        <h2 className={style.sutitle}>ПРОИЗВОДСТВО И&nbsp;ПРОДАЖА ПИЛОМАТЕРИАЛОВ</h2>
+        <a className={style.button} href="#contacts">КУПИТЬ  СЕЙЧАС</a>
+        <p className={style.text}>Стройте свои мечты из&nbsp;пиломатериалов лесов Сибири.</p>
+        <h1 className={style.title}>ЛЕС СИБИРИ</h1>
+      </div>
+      <img className={style.img} src={cover} width={1340} height={544} alt="Фотография леса на фоне реки" />
     </section>
   );
 }
