eb6c2ceca855d558906027ae6599cec656a54908
WadimSh <wadim11@yandex.ru>
2023-03-31 14:57:01 +0300

    fix: удален не используемый код

--- src/components/lumber/lumber.jsx
+++ src/components/lumber/lumber.jsx
@@ -10,10 +10,6 @@ function Lumber({ lumberRef }) {
         <h2 className={style.title}>
           // ПИЛОМАТЕРИАЛЫ
         </h2>
-        {/*<a className={style.link} href="/download/DOC-20230314-WA0016.xlsx" download>
-          СКАЧАТЬ ПРАЙС-ЛИСТ
-          <div className={style.arrow}></div>
-        </a>*/}
       </div>
     <div className={style.cardList}>
       {cards.map((card) =>
