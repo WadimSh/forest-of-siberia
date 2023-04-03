9019c08f43c99fd1ded5805e832b9e8982107af1
WadimSh <wadim11@yandex.ru>
2023-04-03 17:22:13 +0300

    fix: стилизованы страницы со служебной информацией

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -1,6 +1,6 @@
 import Landing from "./landing/landing";
 import Policy from "./policy/policy";
-import Agreement from "./agreement/agreement";
+import Agreement from "./policy/agreement";
 import NotFound from "./not-found/not-found";
 
 export {
