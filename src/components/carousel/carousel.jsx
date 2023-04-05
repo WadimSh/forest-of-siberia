61ed2df4eabc5e52a701278f45af54e959f74810
WadimSh <wadim11@yandex.ru>
2023-04-05 11:15:48 +0300

    feat: сделан компонент слайдера carousel

--- src/components/carousel/carousel.jsx
+++ src/components/carousel/carousel.jsx
@@ -1,8 +1,90 @@
+import { useState } from "react";
+import AliceCarousel from "react-alice-carousel";
+import items from "../../utils/items";
+
+const items = items.map((item, index) => (
+  <img src={item} alt={item} />
+))
+
+const thumbItems = (items, [setThumbIndex, setThumbAnimation]) => {
+  return items.map((item, i) => (
+    <div className="thumb" onClick={() => (setThumbIndex(i), setThumbAnimation(true))}>
+      <img src={item} alt={item} />
+    </div>
+  ))
+};
+
 function Carousel() {
+  const [mainIndex, setMainIndex] = useState(0);
+  const [mainAnimation, setMainAnimation] = useState(false);
+  const [thumbIndex, setThumbIndex] = useState(0);
+  const [thumbAnimation, setThumbAnimation] = useState(false);
+  const [thumbs] = useState(thumbItems(items, [setThumbIndex, setThumbAnimation]));
+
+  const slideNext = () => {
+    if (!thumbAnimation && thumbIndex < thumb.length - 1) {
+      setThumbAnimation(true);
+      setThumbIndex(thumbIndex + 1);
+    }
+  };
+
+  const slidePrev = () => {
+    if (!thumbAnimation && thumbIndex > 0) {
+      setThumbAnimation(true);
+      setThumbIndex(thumbIndex - 1);
+    }
+  };
+
+  const syncMainBeforeChange = (e) => {
+    setMainAnimation(true);
+  };
+
+  const syncMainAfterChange = (e) => {
+    setMainAnimation(false);
+    if (e.type === 'action') {
+      setThumbIndex(e.item);
+      setThumbAnimation(false);
+    } else {
+      setMainIndex(thumbIndex);
+    }
+  };
+
+  const syncThumbs = (e) => {
+    setThumbIndex(e.item);
+    setThumbAnimation(false);
+    if (!mainAnimation) {
+      setMainIndex(e.item);
+    }
+  };
       
   return [
-    
-  ];
+    <AliceCarousel
+      activeIndex={mainIndex}
+      animationType="fadeout"
+      animationDuration={800}
+      disableDotsControls
+      disableButtonsControls
+      items={items}
+      mouseTracking={!thumbAnimation}
+      onSlideChange={syncMainBeforeChange}
+      onSlideChanged={syncMainAfterChange}
+      touchTracking={!thumbAnimation}
+    />,
+    <div className="thumbs">
+      <AliceCarousel
+        activeIndex={thumbIndex}
+        autoWidth
+        disableDotsControls
+        disableButtonsControls
+        items={thumbs}
+        mouseTracking={false}
+        onSlideChanged={syncThumbs}
+        touchTracking={!mainAnimation}
+      />
+      <div className="btn-prev" onClick={slidePrev}>&lang;</div>
+      <div className="btn-next" onClick={slideNext}>&rang;</div>
+    </div>
+  ]
 }
 
 export default Carousel;
---
+++ src/utils/items.js
@@ -0,0 +1,23 @@
+import item1 from '../images/slider/item1.jpg';
+import item2 from '../images/slider/item2.jpg';
+import item3 from '../images/slider/item3.jpg';
+import item4 from '../images/slider/item4.jpg';
+import item5 from '../images/slider/item5.jpg';
+import item6 from '../images/slider/item6.jpg';
+import item7 from '../images/slider/item7.jpg';
+import item8 from '../images/slider/item8.jpg';
+import item9 from '../images/slider/item9.jpg';
+import item10 from '../images/slider/item10.jpg';
+import item11 from '../images/slider/item11.jpg';
+import item12 from '../images/slider/item12.jpg';
+import item13 from '../images/slider/item13.jpg';
+import item14 from '../images/slider/item14.jpg';
+import item15 from '../images/slider/item15.jpg';
+import item16 from '../images/slider/item16.jpg';
+import item17 from '../images/slider/item17.jpg';
+import item18 from '../images/slider/item18.jpg';
+import item19 from '../images/slider/item19.jpg';
+
+const items = [item1, item2, item3, item4, item5, item6, item7, item8, item9, item10, item11, item12, item13, item14, item15, item16, item17, item18, item19];
+
+export default items;
